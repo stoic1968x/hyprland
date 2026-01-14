3c0e6f57805cdb75460770a3494703e3b56fd8de
stoic1968x <costaedu@tutamail.com>
2026-01-14 18:29:14 -0300

    Atualizar arquivos de configuração

--- copy-files.sh
+++ copy-files.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Copiar arquivos de configuração para a pasta do repositório Git
-cp -r ~/.config/swaync ~/Hyprland-Git/.config/swaync
-cp -r ~/.config/hypr ~/Hyprland-Git/.config/hypr
-cp -r ~/.config/pipewire ~/Hyprland-Git/.config/pipewire
-cp -r ~/.config/gtk-3.0 ~/Hyprland-Git/.config/gtk-3.0
-cp -r ~/.config/gtk-4.0 ~/Hyprland-Git/.config/gtk-4.0
-cp -r ~/.config/rofi ~/Hyprland-Git/.config/rofi
-cp -r ~/.config/matugen ~/Hyprland-Git/.config/matugen
-cp -r ~/.config/wlogout ~/Hyprland-Git/.config/wlogout
-cp -r ~/.config/kitty ~/Hyprland-Git/.config/kitty
-cp -r ~/.config/waybar ~/Hyprland-Git/.config/waybar
-cp -r ~/.config/Kvantum ~/Hyprland-Git/.config/Kvantum
+# Usar rsync para copiar arquivos e diretórios
+rsync -av --delete ~/.config/swaync/ ~/Hyprland-Git/.config/swaync/
+rsync -av --delete ~/.config/hypr/ ~/Hyprland-Git/.config/hypr/
+rsync -av --delete ~/.config/pipewire/ ~/Hyprland-Git/.config/pipewire/
+rsync -av --delete ~/.config/gtk-3.0/ ~/Hyprland-Git/.config/gtk-3.0/
+rsync -av --delete ~/.config/gtk-4.0/ ~/Hyprland-Git/.config/gtk-4.0/
+rsync -av --delete ~/.config/rofi/ ~/Hyprland-Git/.config/rofi/
+rsync -av --delete ~/.config/matugen/ ~/Hyprland-Git/.config/matugen/
+rsync -av --delete ~/.config/wlogout/ ~/Hyprland-Git/.config/wlogout/
+rsync -av --delete ~/.config/kitty/ ~/Hyprland-Git/.config/kitty/
+rsync -av --delete ~/.config/waybar/ ~/Hyprland-Git/.config/waybar/
+rsync -av --delete ~/.config/Kvantum/ ~/Hyprland-Git/.config/Kvantum/
 
-cp ~/.zshrc ~/Hyprland-Git/.zshrc
-cp ~/.zprofile ~/Hyprland-Git/.zprofile
-cp ~/.gtkrc-2.0 ~/Hyprland-Git/.gtkrc-2.0
+rsync -av ~/.zshrc ~/Hyprland-Git/.zshrc
+rsync -av ~/.zprofile ~/Hyprland-Git/.zprofile
+rsync -av ~/.gtkrc-2.0 ~/Hyprland-Git/.gtkrc-2.0
 
 # Navegar para o diretório do repositório Git
 cd ~/Hyprland-Git || exit
@@ -23,7 +23,7 @@ cd ~/Hyprland-Git || exit
 # Adicionar os arquivos modificados ao Git
 git add .
 
-# Criar um commit com uma mensagem padrão (você pode personalizar a mensagem conforme necessário)
+# Criar um commit com uma mensagem padrão
 git commit -m "Atualizar arquivos de configuração"
 
 # Enviar as mudanças para o repositório remoto
